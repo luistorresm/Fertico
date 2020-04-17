f50a32de582c3da6bd5224b5219f938dbb4324d7
Noe Moreno <nmoreno@wobin.com.mx>
2020-04-17 10:37:08 -0500

    [DELETE] remove no authoriced aplications

--- fertico-addons/models/__init__.py
+++ fertico-addons/models/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from . import models
-from . import account_analytic
-from . import attendances
-from . import sales_chanel
+#from . import account_analytic
+#from . import attendances
+#from . import sales_chanel
