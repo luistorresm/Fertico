da852c52894b720986fb59c5b2891423357ba80c
Noe Moreno <nmoreno@wobin.com.mx>
2020-05-13 14:57:55 -0500

    [ADD] add coments

--- fertico-addons/models/expiration_mail.py
+++ fertico-addons/models/expiration_mail.py
@@ -9,17 +9,19 @@ class StockProductLot(models.Model):
 
     @api.model
     def mailmessage(self):
-        vals = {}
-        domain=[('name','=','caducidad')]
-        tplate = self.env['mail.template'].search([('name','=',"caducidad")])
-        for temp in tplate:
-            print('Template', temp.name)
-            template = temp.id
-        if template:
+        '''This method send an email in the alert day of lots and serial numbers'''
+
+        #search the email template
+        template = self.env['mail.template'].search([('name','=',"caducidad")])
+        for temp in template:
+            template_id = temp.id
+        if template_id:
+            #search the lots while the alert day is now
             now = datetime.now()
             lots=self.env['stock.production.lot'].search(['&',('alert_date','<',str(now)),('notified','=',False)])
             for lot in lots:
-                tplate.send_mail(lot.id, True)
+                #send the emails and change the notified to True
+                template.send_mail(lot.id, True)
                 lot.write({
                     'notified': True
                 })
