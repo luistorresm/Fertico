9b38205ef58e9d6d3e2c655cc8848d78b7028a9d
Noe Moreno <nmoreno@wobin.com.mx>
2020-05-15 11:04:18 -0500

    [ADD] add track visibility

--- block-credit/models/tracking_limit.py
+++ block-credit/models/tracking_limit.py
@@ -5,3 +5,5 @@ class ResPartner(models.Model):
     _inherit='res.partner'
 
     credit_limit = fields.Float(track_visibility='onchange')
+    allowed_sale = fields.Boolean(track_visibility='onchange')
+    grace_payment_days = fields.Float(track_visibility='onchange')
