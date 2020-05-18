ffdc4e8ac3d85fc663fd43913b70644a5d99e264
Noe Moreno <nmoreno@wobin.com.mx>
2020-05-18 10:57:14 -0500

    [ADD] add report credit limit

--- block-credit/models/tracking_limit.py
+++ block-credit/models/tracking_limit.py
@@ -7,3 +7,12 @@ class ResPartner(models.Model):
     credit_limit = fields.Float(track_visibility='onchange')
     allowed_sale = fields.Boolean(track_visibility='onchange')
     grace_payment_days = fields.Float(track_visibility='onchange')
+    website = fields.Char(track_visibility='onchange')
+
+class MailTrackingValue(models.Model):
+
+    _inherit='mail.tracking.value'
+    
+    author = fields.Char(string='User', related='mail_message_id.author_id.name')
+    contact = fields.Char(string='Contact', related='mail_message_id.record_name')
+    date_change = fields.Datetime(string='Date', related='mail_message_id.date')
