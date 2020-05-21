5b78234f04651d0b2cb90f07f69549144083b365
Noe Moreno <nmoreno@wobin.com.mx>
2020-05-21 12:11:31 -0500

    [ADD] add security model

--- block-credit/__manifest__.py
+++ block-credit/__manifest__.py
@@ -26,7 +26,7 @@
 
     # always loaded
     'data': [
-        #'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'security/credit_security.xml',
         'views/views.xml',
         'views/templates.xml',
