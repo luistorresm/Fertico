022b1a9b29c618475c4907aa6fb380b7c3444fe4
Noe Moreno <nmoreno@wobin.com.mx>
2020-05-21 10:24:30 -0500

    [FIX] fix track company

--- block-credit/models/tracking_limit.py
+++ block-credit/models/tracking_limit.py
@@ -13,18 +13,19 @@ class MailTrackingValue(models.Model):
 
     _inherit='mail.tracking.value'
 
-    def _user_company(self):
-        comp=self.env.user.company_id.name
-        return comp
-
-    def _user_name(self):
-        user_name=self.env.user.name
-        return user_name
     
-    author = fields.Char(string='User', default=_user_name)
+    author = fields.Char(string='User', related='mail_message_id.author_id.name', store=True)
     contact = fields.Char(string='Contact', related='mail_message_id.record_name', store=True)
     date_change = fields.Datetime(string='Date', related='mail_message_id.date', store=True)
-    company_user=fields.Char(string="Company", default=_user_company)
+    company_user = fields.Char(string="Company")
+
+    @api.model
+    def create(self, values):
+        track = super(MailTrackingValue, self).create(values)
+        track.company_user=track.mail_message_id.author_id.company_id.name
+        return track
+
+
 
     
 
