aea7c5eddd63acd692a0ff79d6cbaa10c7ff3560
Noe Moreno <nmoreno@wobin.com.mx>
2020-06-11 12:56:07 -0500

    [FIX] fix repeat vat

--- fertico-addons/models/contact.py
+++ fertico-addons/models/contact.py
@@ -3,31 +3,39 @@ from odoo import models, fields, api
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    '''
-    @api.model
-    def write(self, values):
-        if values.vat:
-            dup_ids = self.env['hr.employee'].search(['&','&', ('vat', '=', values.vat), ('id', '!=', values.id), ('id', 'not in', values.child_ids.ids)])
-            if dup_ids:
-                raise Warning('RFC debe ser unico, este ya ha sido registrado!')
-        
-        contact = super(ResPartner, self).write(values)
-        return contact
-        '''
-
     @api.model
     def create(self, values):
-        print("============00",values['vat'])
-        
+        if values.get('parent_id'):
+            if values.get('vat'):
+                if values['vat']:
+                    if values['vat'] != "XAXX010101000":
+                        dup_ids = self.env['res.partner'].search([('vat', '=', values['vat'])])
+                        if dup_ids:
+                            values['vat']=''
+        else:
+            if values.get('vat'):
+                if values['vat']:
+                    if values['vat'] != "XAXX010101000":
+                        dup_ids = self.env['res.partner'].search([('vat', '=', values['vat'])])
+                        if dup_ids:
+                            raise Warning('RFC debe ser unico, este ya ha sido registrado!')
+            
         contact = super(ResPartner, self).create(values)
 
-        '''if contact.vat != '':
-            dup_ids = self.env['res.partner'].search([('vat', '=', contact.vat)])
-            if dup_ids:
-                raise Warning('RFC debe ser unico, este ya ha sido registrado!')
-        else:
-            print("entrooooooooooooooooooo aqui")
-        '''
+        return contact
+
+    @api.multi
+    def write(self, values):
+
+        if values.get('vat'):
+            if values['vat']:
+                if values['vat'] != "XAXX010101000":
+                    dup_ids = self.env['res.partner'].search([('vat', '=', values['vat'])])
+                    if dup_ids:
+                        values['vat']=''
+            
+        contact = super(ResPartner, self).write(values)
+
         return contact
         
         
