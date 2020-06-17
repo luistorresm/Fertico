b33caafc4cf5d93bf2d4e70498306f891a3508cb
Noe Moreno <nmoreno@wobin.com.mx>
2020-06-17 11:04:51 -0500

    [FIX] fix invoice domain

--- fertico-addons/models/account_invoice_correction.py
+++ fertico-addons/models/account_invoice_correction.py
@@ -3,4 +3,4 @@ from odoo import fields, models, api
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    user_id = fields.Many2one(readonly=False)
+    user_id = fields.Many2one(readonly=False, domain="[('sale_team_id','=',team_id)]")
