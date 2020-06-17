8083c7ef6f90d064fc05366f409fb5db0359054d
Noe Moreno <nmoreno@wobin.com.mx>
2020-06-17 18:56:19 -0500

    [ADD] add required

--- fertico-addons/models/account_invoice_correction.py
+++ fertico-addons/models/account_invoice_correction.py
@@ -3,6 +3,7 @@ from odoo import fields, models, api
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    user_id = fields.Many2one(readonly=False)
+    user_id = fields.Many2one(readonly=False, required=True)
+    team_id = fields.Many2one(required=True)
 
     
