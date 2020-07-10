90c2c57fd8f7b0b70fec0a93aff288aabf1da3e1
Sebastian-AM-Wobin <omarsebastian100@gmail.com>
2020-07-10 10:25:32 -0500

    [ADD] Mejoras en código, cometnarios y finalización de ticket

--- fertico-addons/models/analytic_tag_analysis.py
+++ fertico-addons/models/analytic_tag_analysis.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models, _
 
-#//////////////////////////////////////////////////////////////////////////////////////////////#
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
 #   TICKET 102    DEVELOPED BY SEBASTIAN MENDEZ    --     START
 # 
 # This ticket intends to help final users to clasify and to add new grouping tools in order
 # to improve their analysis experience,
-#//////////////////////////////////////////////////////////////////////////////////////////////#
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
 class AccountAnalyticTag(models.Model):
     _inherit = "account.analytic.tag"
 
-    #////////////////////////////////////////////////////////////////////////
+    #:::::::::::::::::::::::::::::::::::::::::::::
     #   MODEL FIELDS
-    #////////////////////////////////////////////////////////////////////////    
+    #:::::::::::::::::::::::::::::::::::::::::::::       
     analytic_tag_type = fields.Selection([('trip', 'Trip'),
                                           ('route', 'Route'),
                                           ('operator', 'Operator')],
@@ -21,19 +21,21 @@ class AccountAnalyticTag(models.Model):
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
-    #////////////////////////////////////////////////////////////////////////
+    #:::::::::::::::::::::::::::::::::::::::::::::
     #   MODEL FIELDS
-    #////////////////////////////////////////////////////////////////////////
+    #:::::::::::::::::::::::::::::::::::::::::::::
     tag_trip = fields.Char('Trip', compute="_compute_trip")    
     tag_route = fields.Char('Route', compute="_compute_route")    
     tag_operator = fields.Char('Operator', compute="_compute_operator")   
 
 
-    #////////////////////////////////////////////////////////////////////////
+    #:::::::::::::::::::::::::::::::::::::::::::::
     #   MODEL METHODS
-    #////////////////////////////////////////////////////////////////////////
+    #:::::::::::::::::::::::::::::::::::::::::::::
     @api.depends('analytic_tag_ids')
     def _compute_trip(self):
+        """This method intends to distribute and assign trips in analytic tags in
+           their corresponding columns in account move lines"""
         for rec in self:
             #Obtain multiples IDs:
             if rec.analytic_tag_ids:
@@ -50,6 +52,8 @@ class AccountMoveLine(models.Model):
 
     @api.depends('analytic_tag_ids')
     def _compute_route(self):
+        """This method intends to distribute and assign routes in analytic tags in
+           their corresponding columns in account move lines"""        
         for rec in self:
             #Obtain multiples IDs:
             if rec.analytic_tag_ids:
@@ -66,6 +70,8 @@ class AccountMoveLine(models.Model):
 
     @api.depends('analytic_tag_ids')
     def _compute_operator(self):
+        """This method intends to distribute and assign operators in analytic tags in
+           their corresponding columns in account move lines"""        
         for rec in self:
             #Obtain multiples IDs:
             if rec.analytic_tag_ids:
@@ -78,6 +84,6 @@ class AccountMoveLine(models.Model):
                     #Assign value into column of operator
                     if tag.analytic_tag_type == 'operator':
                         rec.tag_operator = tag.name             
-#//////////////////////////////////////////////////////////////////////////////////////////////#
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
 #   TICKET 102    DEVELOPED BY SEBASTIAN MENDEZ    --     END
-#//////////////////////////////////////////////////////////////////////////////////////////////#
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
