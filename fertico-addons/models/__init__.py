945b23de916e87ffc3f3b500a370228b31f219de
Sebastian-AM-Wobin <omarsebastian100@gmail.com>
2020-07-22 11:48:14 -0500

    [ADD] Agregación de nueva columna de fecha de factura

--- fertico-addons/models/__init__.py
+++ fertico-addons/models/__init__.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from . import models
+from . import account_aged_receivable_inherit
 from . import account_analytic
+from . import account_invoice_correction
 from . import attendances
-from . import sales_chanel
-from . import taxes
-from . import pos_init
-from . import store_term
-from . import price_list
 from . import contact
 from . import domain_terms
-from . import account_invoice_correction
+from . import pos_init
+from . import price_list
+from . import sales_chanel
+from . import store_term
+from . import taxes
---
+++ fertico-addons/models/account_aged_receivable_inherit.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from odoo import models, fields, api
+from odoo.tools.translate import _  
+
+#\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\#
+#   TICKET 011 ALBAGRO    DEVELOPED BY SEBASTIAN MENDEZ    --     START
+#\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\#
+class report_account_aged_receivable(models.AbstractModel):
+    _inherit = "account.aged.receivable"
+
+    def get_columns_name(self, options):    
+        """This method intends add a new column of Invoice Dates into Account Aged Receivable Report"""
+        #Obtain columns of original header:
+        columns = super()._get_columns_name(options)
+        #Inserting new column of Invoice Date:
+        columns.insert(1, {'name': _('Invoice Date'), 'class': 'number', 'style': 'white-space:nowrap;'})
+        return columns
+    
+    @api.model
+    def get_lines(self, options, line_id=None):
+        """This method intends add a new values of Invoice Dates into Account Aged Receivable Report"""
+        #Obtain original list of rows of the report:
+        lines = super()._get_lines(options, line_id)
+        for line in lines:
+            #Inject an empty value into the new added column:
+            if line['level'] == 2:
+                line.get('columns').insert(1, {'name': ''}) 
+            #Only the rows with level 4 correspond to broken down concepts:                
+            elif line['level'] == 4:     
+                #Retrieve id of account move, after obtain invoice and its date:
+                move_id = self.env['account.move.line'].browse(line['id']).move_id
+                invoice_id = self.env['account.invoice'].search([('move_id', '=', move_id.id)])
+                #Inserting new value for column of Invoice Date:
+                if invoice_id.date_invoice:
+                    line['columns'].insert(1, {'name': invoice_id.date_invoice.strftime("%d-%m-%Y")})
+                else:
+                    line['columns'].insert(1, {'name': ""})
+        return lines
+#\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\#
+#   TICKET 011 ALBAGRO    DEVELOPED BY SEBASTIAN MENDEZ    --     START
+#\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\#        
