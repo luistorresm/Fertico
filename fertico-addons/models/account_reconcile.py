28f9b4e02541f6f636f70d526464d8aaee4c1950
Noe Moreno <nmoreno@wobin.com.mx>
2020-07-22 09:50:56 -0500

    [ADD] add comments

--- fertico-addons/models/account_reconcile.py
+++ fertico-addons/models/account_reconcile.py
@@ -2,8 +2,11 @@ from odoo import models, fields, api
 
 class AccountPartialReconcile(models.Model):
     _inherit='account.partial.reconcile'
-
+    #==========================================Reporte account reconcile======================================
     
+    #====Metodo para campos calculados, busca los registros anteriores de la factura del registro actual======
+    #====Los suma para para saber el total pagado antes de ese abono y suma el abono actual para tener =======
+    #====el saldo final hasta ese momento=====================================================================
     @api.multi
     @api.depends('amount')
     def get_paid(self):
@@ -15,10 +18,11 @@ class AccountPartialReconcile(models.Model):
             partial.incial=partial.total-paid_total
             partial.final=partial.total-(paid_total+partial.amount)
 
-    
-    total = fields.Monetary(related='debit_move_id.invoice_id.amount_total')
+    #Campos calculados
     incial = fields.Float(string="Saldo inicial", compute='get_paid')
     final = fields.Float(string="Saldo final", compute='get_paid')
+    #Campos relacionados
+    total = fields.Monetary(related='debit_move_id.invoice_id.amount_total')
     invoice = fields.Many2one(related='debit_move_id.invoice_id', store=True)
     customer = fields.Many2one(string="Customer", related='debit_move_id.invoice_id.partner_id', store=True)
     account = fields.Many2one(string="Account", related='debit_move_id.invoice_id.account_id')
