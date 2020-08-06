387969ed6cbd2d820c4607dc5be5a07b90378f36
CSE Sebastián Ayala Méndez <62626813+Sebastian-AM-Wobin@users.noreply.github.com>
2020-08-06 10:38:08 -0500

    Update saldos_pendientes.py

--- albagro_reciba_customs/models/saldos_pendientes.py
+++ albagro_reciba_customs/models/saldos_pendientes.py
@@ -2,6 +2,8 @@
 from odoo import models, fields, api
 from odoo.exceptions import UserError
 from odoo.tools.translate import _
+import logging
+_logger = logging.getLogger(__name__)
 
 
 class PurchaseOrder(models.Model):
@@ -14,6 +16,12 @@ class PurchaseOrder(models.Model):
         sql_query = """SELECT company_id, SUM(residual_signed) FROM account_invoice WHERE (partner_id = %s) AND (state = 'open') AND (type = 'in_invoice' OR type = 'in_refund') GROUP BY company_id;"""
         self.env.cr.execute(sql_query, (self.partner_id.id,))
         residual_companies = self.env.cr.fetchall()
+        _logger.info('\n\n\n residual_companies: %s\n\n\n', residual_companies)
+        
+        sql_query = """SELECT company_id, SUM(residual_signed) FROM account_invoice WHERE (partner_id = %s) AND (type = 'in_invoice' OR type = 'in_refund') GROUP BY company_id;"""
+        self.env.cr.execute(sql_query, (self.partner_id.id,))
+        residual_companies2 = self.env.cr.fetchall()
+        _logger.info('\n\n\n residual_companies2: %s\n\n\n', residual_companies2)        
 
         msg = ""        
         contacts_obj = self.env['res.partner']
@@ -21,6 +29,7 @@ class PurchaseOrder(models.Model):
         if residual_companies:
             #Construct the error message:
             debtor = contacts_obj.search([('id', '=', self.partner_id.id)]).name
+            _logger.info('\n\n\n debtor: %s\n\n\n', debtor)
             msg = _('The related contact on the purchase order %s has outstanding balances on sales: \n') % (debtor)
             #Iterate companies with residual and concatenate:
             for val in residual_companies:
