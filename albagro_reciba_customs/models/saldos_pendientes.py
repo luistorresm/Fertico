3c7757ff414cc09d1ea4e08f261371bfef7411da
Sebastian-AM-Wobin <omarsebastian100@gmail.com>
2020-08-06 10:26:13 -0500

    [FIX] Correcciones en consulta, mejora de mensaje y traducciones

--- albagro_reciba_customs/models/saldos_pendientes.py
+++ albagro_reciba_customs/models/saldos_pendientes.py
@@ -2,8 +2,7 @@
 from odoo import models, fields, api
 from odoo.exceptions import UserError
 from odoo.tools.translate import _
-import logging
-_logger = logging.getLogger(__name__)
+
 
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
@@ -12,21 +11,24 @@ class PurchaseOrder(models.Model):
     def button_confirm(self):
         #Retrieve rows from "account.invoice" model where the present provider
         #is revised to detect if has open invoices indicating that must pay its debts: 
-        sql_query = """SELECT company_id, SUM(residual_signed) FROM account_invoice WHERE type = 'in_invoice' OR type = 'in_refund' GROUP BY company_id;"""
-        self.env.cr.execute(sql_query)
+        sql_query = """SELECT company_id, SUM(residual_signed) FROM account_invoice WHERE (partner_id = %s) AND (state = 'open') AND (type = 'in_invoice' OR type = 'in_refund') GROUP BY company_id;"""
+        self.env.cr.execute(sql_query, (self.partner_id.id,))
         residual_companies = self.env.cr.fetchall()
-        _logger.info('\n\n\n residual_companies: %s\n\n\n', residual_companies)
+
+        msg = ""        
+        contacts_obj = self.env['res.partner']
         
         if residual_companies:
             #Construct the error message:
-            msg = ""
-            msg = _('The related contact on the purchase order has outstanding balances on sales: \n')
+            debtor = contacts_obj.search([('id', '=', self.partner_id.id)]).name
+            msg = _('The related contact on the purchase order %s has outstanding balances on sales: \n') % (debtor)
             #Iterate companies with residual and concatenate:
             for val in residual_companies:
-                msg += _('\n%s balance by $%s') % (val[0], val[1]) 
-                #Concatenate last part of error message:
-                msg += _('\nconsider making discounts for settlement.')
-                _logger.info('\n\n\n %s \n\n\n', msg) 
+                company_name = contacts_obj.search([('id', '=', val[0])]).name
+                msg += _('\n[+] %s balance by $%s') % (company_name, val[1]) 
+
+            #Concatenate last part of error message:
+            msg += _('\n\nConsider making discounts for settlement.')
 
             #Display error message:
             raise UserError(msg)
