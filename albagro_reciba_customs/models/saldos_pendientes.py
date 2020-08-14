ad8ac12f1f7eade32cd4500ee2dfc6e673e37a26
Sebastian-AM-Wobin <omarsebastian100@gmail.com>
2020-08-14 12:52:56 -0500

    [ADD] Nuevos cambios en validaciones, alerta, selección de descuentos

--- albagro_reciba_customs/models/saldos_pendientes.py
+++ albagro_reciba_customs/models/saldos_pendientes.py
@@ -3,8 +3,6 @@ from odoo import models, fields, api
 from odoo.addons import decimal_precision as dp
 from odoo.exceptions import UserError
 from odoo.tools.translate import _
-import logging
-_logger = logging.getLogger(__name__)
 
 #\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\
 
@@ -20,66 +18,51 @@ _logger = logging.getLogger(__name__)
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
-    selected_sl_inv = fields.Boolean(string='Selected Discount', store=True) 
+    selected_sl_inv  = fields.Boolean(string='Selected Discount') 
+    assigned_pur_ord = fields.Char(string='Assigned Purchase Order') 
 
-    @api.onchange('selected_sl_inv')
-    def _onchange_selected_sl_inv(self):
-        self.ensure_one()
-        _logger.info('\n\n\n ID FACTURA: %s\n\n\n', self._origin.id)        
-                        
-        #Update trial 1:
-        invoice_recordset = self.env['account.invoice'].browse(self._origin.id)
-        invoice_recordset.selected_sl_inv = True
-        _logger.info('\n\n\n invoice_recordset.selected_sl_inv: %s\n\n\n', invoice_recordset.selected_sl_inv)
-        
-        #Update trial 2:
-        values = {'selected_sl_inv': True}
-        recordset = self.env['account.invoice'].write(values)
-        _logger.info('\n\n\n recordset: %s\n\n\n', recordset)   
-
+    def change_selected_sl_inv(self):           
+        '''This method permits to change status of checkbox and assigning a purchase order'''
+        if self.selected_sl_inv:
+            values = {'selected_sl_inv': False, 'assigned_pur_ord': ''}
+            self.write(values)
+        else:
+            values = {'selected_sl_inv': True, 'assigned_pur_ord': self.id}
+            self.write(values)  
 
 
 
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
-    pending_sales_invoices_ids = fields.One2many('account.invoice', 'partner_id', 
-                                                 string='Pending Sales Invoices',
+    pending_sales_invoices_ids = fields.One2many('account.invoice', 'partner_id', string='Pending Sales Invoices',
                                                  compute='get_invoices')   
-    
-    ammount_sl_pending_inv = fields.Float(string='Ammount of Pedinng Sales Invoices', 
-                                          digits=dp.get_precision('Product Unit of Measure'),
-                                          compute='sum_residual_signed') 
-
-    ammount_select_discounts = fields.Float(string='Ammount of Selected Discounts', 
-                                            digits=dp.get_precision('Product Unit of Measure'))     
-
+    ammount_sl_pending_inv     = fields.Float(string='Ammount of Pedinng Sales Invoices', digits=dp.get_precision('Product Unit of Measure'),
+                                              compute='sum_residual_signed') 
+    ammount_select_discounts   = fields.Float(string='Ammount of Selected Discounts', digits=dp.get_precision('Product Unit of Measure'),
+                                              compute='sum_select_discounts')     
 
     def get_invoices(self):       
-        '''Fill new One2Many field with debted bills belonging to a client,
-           determining its state as open:''' 
+        '''Fill new One2Many field with debted bills belonging to a client, determining its state as open:''' 
         domain=[('state', '=', 'open'), 
                 ('partner_id', '=', self.partner_id.id),
                 '|', ('type', '=', 'in_invoice'),
                      ('type', '=', 'in_refund')]                                                                     
         self.pending_sales_invoices_ids = self.env['account.invoice'].search(domain)                                            
 
+
     @api.multi
     def sum_residual_signed(self):
-        '''Sumatory of pending sales invoices'''
+        '''Sumatory for ammount of pending sales invoices'''
         for rec in self:
-            rec.ammount_sl_pending_inv = sum(line.residual_signed for line in rec.pending_sales_invoices_ids)                                            
-    
-    @api.onchange('pending_sales_invoices_ids')
-    def determine_discounts(self):
-        invoice_id = self._origin.id
-        _logger.info('\n\n\n invoice_id : %s\n\n\n', invoice_id)
-        #Update trial 3:
-        self.ensure_one()
-        invoice_recordset = self.env['account.invoice'].browse(invoice_id)
-        invoice_recordset.selected_sl_inv = True                       
-              
-        self.ammount_select_discounts = sum(line.residual_signed for line in self.pending_sales_invoices_ids if line.selected_sl_inv == True)         
+            rec.ammount_sl_pending_inv = sum(line.residual_signed for line in rec.pending_sales_invoices_ids)
+
+
+    @api.multi
+    def sum_select_discounts(self):
+        '''Determine the ammount of selected sales invoices to charge by operator'''
+        for rec in self:
+            rec.ammount_select_discounts = sum(line.residual_signed for line in rec.pending_sales_invoices_ids if line.selected_sl_inv == True)
 
 
     #/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/
@@ -88,59 +71,45 @@ class PurchaseOrder(models.Model):
     @api.multi
     def button_confirm(self): 
         #Initialize variables:
-        msg = ""; summatory_residual = 0;
+        msg = ""; summatory_residual = 0
 
         #Retrieve rows from "account.invoice" model where the present provider
         #is revised to detect if has open invoices indicating that must pay its debts:         
         sql_query = """SELECT company_id, SUM(residual_signed) 
-                       FROM account_invoice 
-                       WHERE (partner_id = %s)
-                         AND (state = 'open')
-                         AND (type = 'in_invoice' OR type = 'in_refund') 
-                       GROUP BY company_id;"""
-
+                         FROM account_invoice 
+                        WHERE (partner_id = %s)
+                          AND (state = 'open')
+                          AND (type = 'in_invoice' OR type = 'in_refund') 
+                        GROUP BY company_id;"""
+                        
         self.env.cr.execute(sql_query, (self.partner_id.id,))
-        residual_companies = self.env.cr.fetchall()
-
-        
+        residual_companies = self.env.cr.fetchall()        
 
         #Validate if query has results:
         if residual_companies:           
-            #Construct the error message, beginning with client with open invoices:
+            #Construct the error message, beginning with client with open sales invoices:
             debtor = self.env['res.partner'].search([('id', '=', self.partner_id.id)]).name
             msg = _('The related contact on the purchase order %s has outstanding balances on sales: \n') % (debtor)
-            
-            if residual_companies:                
-                for val in residual_companies:
-                    #Iterate companies with residual (outstanding balance) and concatenate
-                    company_name = self.env['res.company'].search([('id', '=', val[0])]).name
-                    msg += _('\n[+] %s balance by $%s') % (company_name, val[1])
-                    
-                    #Sum up residuals in order to indicate how much is debted
-                    summatory_residual += val[1] 
-             
-            
+                          
+            for val in residual_companies:
+                #Iterate companies with residual (outstanding balance) and concatenate
+                company_name = self.env['res.company'].search([('id', '=', val[0])]).name
+                msg += _('\n[+] %s balance by $%s') % (company_name, val[1])                    
+                #Sum up residuals in order to indicate how much is debted
+                summatory_residual += val[1] 
+                         
             #Concatenate last part of error message:
             msg += _('\n\nConsider making discounts for settlement.')
 
             #Error message must be omitted when a given user has permission to invalidate it
-            #being this indicated by a checkbox in res.partner:
+            #being this indicated by a checkbox in "res.users" or when a simple user has indicated
+            #all pending sales invoices were paid otherwise this error message must be displayed:
             uid = self.env.user.id
-            name_user = self.env['res.users'].search([('id', '=', uid)]).name        
-
-            #is_valid_user_flag = self.pool.get('res.users').has_group(cr, uid, 'base.group_omit_balances') 
-            #user = self.env['res.users'].browse(self._uid)
-            #is_valid_user_flag = user.has_group('base.group_omit_balances')            
+            name_user = self.env['res.users'].search([('id', '=', uid)]).name                   
             is_valid_user_flag = self.env.user.has_group('albagro_reciba_customs.group_omit_validation_pending_invoices')         
-
-            _logger.info('\n\n\n uid: %s\n\n\n', uid)            
-            _logger.info('\n\n\n is_valid_user_flag: %s\n\n\n', is_valid_user_flag)            
-            _logger.info('\n\n\n summatory_residual: %s\n\n\n', summatory_residual)
-            _logger.info('\n\n\n ammount_select_discounts: %s\n\n\n', self.ammount_select_discounts)
-
         
-            if is_valid_user_flag == False or not summatory_residual: #or self.ammount_select_discounts:            
-                #Display error message to user like a pop up window:
+            if is_valid_user_flag == False and (summatory_residual != self.ammount_select_discounts):            
+                #Display error message to user like a pop up alert:
                 raise UserError(msg)
             else:
                 #Construction of post's content in Purchase Order Chatter:
