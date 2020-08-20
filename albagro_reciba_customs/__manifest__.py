5997c9ad2849806c25c63c3d56a76b8de80386ed
Noe Moreno <nmoreno@wobin.com.mx>
2020-08-20 12:23:17 -0500

    [ADD] i18n

--- albagro_reciba_customs/__manifest__.py
+++ albagro_reciba_customs/__manifest__.py
@@ -13,7 +13,7 @@
     # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '11.0.1.0.1',
+    'version': '11.0.1.0.4',
 
     # any module necessary for this one to work correctly
     'depends': ['base',
