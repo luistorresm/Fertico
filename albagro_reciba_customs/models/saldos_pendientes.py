beb482086b3798015c3f94268b2963c8ad52fe09
Sebastian Mendez <sebastian-mendez@outlook.com>
2020-09-03 11:56:38 -0500

    [ADD] Agregación de columna de semilla/producto

--- albagro_reciba_customs/models/saldos_pendientes.py
+++ albagro_reciba_customs/models/saldos_pendientes.py
@@ -20,7 +20,7 @@ class AccountInvoice(models.Model):
 
     selected_sl_inv   = fields.Boolean(string='Descuento Seleccionado') 
     #assigned_pur_ord  = fields.Integer(string='Orden de Compra Asignada') 
-    seed_id = fields.Char(string='Semilla' compute='get_product_id')
+    seed_id = fields.Char(string='Semilla/Producto' compute='get_product_id')
     amount_compensate = fields.Float(string='Monto de Compensación', digits=dp.get_precision('Product Unit of Measure'), compute='set_amount_comp')
     amount_transfer   = fields.Float(string='Monto de Transferencia', digits=dp.get_precision('Product Unit of Measure'), compute='set_amount_dif')    
                                    
@@ -36,7 +36,7 @@ class AccountInvoice(models.Model):
     @api.one
     @api.depends('number')
     def get_product_id(self):
-        self.seed_id = self.env['account.invoice.line'].search([('id', '=', invoice_id.id)], limit=1).name
+        self.seed_id = self.env['account.invoice.line'].search([('invoice_id', '=', self.id)], limit=1).name
     
     @api.one
     @api.depends('number')
