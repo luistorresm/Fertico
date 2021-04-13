da7bfcf2839613e255ce92c7271aaad3928d419d
Noe Moreno <nmoreno@wobin.com.mx>
2021-04-13 12:57:40 -0500

    [ADD] parametros

---
+++ wobin_credit/models/params.py
@@ -0,0 +1,17 @@
+from odoo import models, fields, api
+
+class CreditParameters(models.Model):
+    _name = "credit.parameters"
+
+    name = fields.Char(string="Nombre")
+    crop_type = fields.Many2one('product.product', string="Tipo de cultivo")
+    crop_method = fields.Selection([('irrigation', 'Riego'),('rainwater', 'Temporal')], string="Metodo de cultivo")
+    amount = fields.Float("Monto por hectarea ($)") 
+
+class CreditTypes(models.Model):
+    _name = "credit.types"
+
+    name = fields.Char(string="Tipo de crédito")
+    payment_terms = fields.Integer(string="Plazo de pago")
+    interest = fields.Float(string="Interes")
+    interest_mo = fields.Float(string="Interes moratorio")
