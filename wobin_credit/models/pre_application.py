0748c42244997171926d6c95b13dfa7829af41e6
Noe Moreno <nmoreno@wobin.com.mx>
2021-04-20 16:19:12 -0500

    [ADD] modulo

---
+++ wobin_credit/models/pre_application.py
@@ -0,0 +1,67 @@
+from odoo import models, fields, api
+
+class CreditPreApplication(models.Model):
+    _name = "credit.preapplication"
+    _inherit = ['mail.thread']
+
+    @api.depends('crop_type','crop_method','hectares')
+    def get_amount(self):
+        amount = 0
+        insurance = 0
+        
+        if self.crop_type and self.crop_method:
+            param = self.env['credit.parameters'].search([('crop_type','=',self.crop_type.id),('crop_method','=',self.crop_method)])
+            if param:
+                amount = param.amount*self.hectares
+                insurance = param.insurance*self.hectares
+
+
+        self.calculated_amount = amount
+        self.insurance = insurance
+
+    def _get_name(self):
+        count = self.env['credit.preapplication'].search([('company_id','=',self.env.user.company_id.id)])
+        number = str(len(count)+1).zfill(4)
+        return 'PRE-'+number
+
+
+    state = fields.Selection([('draft', 'Borrador'),
+    ('locked', 'Bloqueado')], default='draft')
+    company_id = fields.Many2one('res.company', default=lambda self: self.env['res.company']._company_default_get('credit.preapplication'))
+    name = fields.Char('Preaplicación', default=_get_name, readonly=True)
+    partner_id = fields.Many2one('res.partner', string="Cliente")
+    cycle =  fields.Many2one('credit.cycles', string="Ciclo")
+    crop_type = fields.Many2one('product.product', string="Tipo de cultivo")
+    crop_method = fields.Selection([('irrigation', 'Riego'),('rainwater', 'Temporal')], string="Metodo de cultivo")
+    hectares = fields.Float(string="Hectareas")
+    calculated_amount = fields.Float(string="Monto permitido", compute="get_amount", store=True)
+    requested_amount = fields.Float(string="Monto solicitado")
+    authorized_amount = fields.Float(string="Monto autorizado")
+    insurance = fields.Float(string="Seguro Agrícola", compute="get_amount", store=True)
+    credit_type_id = fields.Many2one('credit.types', string="Tipo de crédito")
+    payment_terms = fields.Many2one(related='credit_type_id.payment_terms', string="Plazo de pago", readonly='True')
+    date_limit_flag = fields.Boolean(default="False")
+    date_limit = fields.Date(string="Fecha límite")
+    interest = fields.Float(related='credit_type_id.interest', string="Interés", readonly='True')
+    interest_mo = fields.Float(related='credit_type_id.interest_mo', string="Interés moratorio", readonly='True')
+
+    @api.onchange('payment_terms')
+    def get_payment_term(self):
+
+        if self.payment_terms and len(self.payment_terms.line_ids) > 1:
+            
+            if self.payment_terms.line_ids[1].days == 180:
+                self.date_limit_flag = True
+            else:
+                self.date_limit_flag = False
+                self.date_limit = ''
+        else:
+            self.date_limit_flag = False
+            self.date_limit = ''
+
+    def lock_credit(self):
+
+        self.state = 'locked'
+
+
+    
---
+++ wobin_credit/models/reports.py
@@ -0,0 +1,93 @@
+from odoo import models, fields, api
+from datetime import date, datetime
+from odoo.exceptions import UserError
+
+class CreditAccountStatus(models.TransientModel):
+    #Recepciones por facturar    
+    _name='credit.account.status'
+    
+    partner_id = fields.Many2one('res.partner', string="Cliente")
+    date = fields.Date(string="Fecha de cálculo")
+
+class ReportAccountStatus(models.AbstractModel):
+    #Reporte recepciones por facturar
+    _name = 'report.wobin_credit.report_account_status'
+
+    @api.model
+    def get_report_values(self, docids, data=None):
+        report = self.env['credit.account.status'].browse(docids)
+        credit = self.env['credit.preapplication'].search([('partner_id','=',report.partner_id.id)], limit=1)
+        invoices = self.env['account.invoice'].search([('partner_id','=',report.partner_id.id),('type','=','out_invoice'),('state','=','open')])
+        date_payment = datetime.strptime(report.date, '%Y-%m-%d')
+        inv_data = []
+        total = 0
+        sum_invoices = 0
+        sum_interest = 0
+
+        for invoice in invoices:
+            interest = invoice.amount_total*(credit.interest/100)
+            interest_mo = 0
+            date_invoice = datetime.strptime(invoice.date, '%Y-%m-%d')
+            term = credit.payment_terms.line_ids[1].days
+            days = 0
+            days_limit = 0
+
+            if term == 30:
+                days = (date_payment - date_invoice).days
+            
+                if  days > 30 and days <= 60:
+                    interest = ((invoice.amount_total*(credit.interest/100))/30)*(days-30)
+                elif days > 60:
+                    interest = ((invoice.amount_total*(credit.interest/100))/30)*(days-30)
+                    interest_mo = ((invoice.amount_total*(credit.interest_mo/100))/30)*(days-60)
+            
+            elif term == 180:
+                days = (date_payment - date_invoice).days
+                date_limit = datetime.strptime(credit.date_limit, '%Y-%m-%d')
+                days_limit = (date_limit - date_invoice).days
+
+                if  date_payment <= date_limit:
+                    interest = ((invoice.amount_total*(credit.interest/100))/30)*(days)
+                elif date_payment > date_limit:
+                    interest = ((invoice.amount_total*(credit.interest/100))/30)*(days_limit)
+                    interest_mo = ((invoice.amount_total*(credit.interest_mo/100))/30)*(days-days_limit)
+            
+            total_inv = invoice.amount_total+interest+interest_mo
+            total += total_inv
+            sum_invoices += invoice.amount_total
+            sum_interest += interest + interest_mo
+
+            inv = {
+                'number': invoice.number,
+                'date': date_invoice.strftime("%d/%m/%Y"),
+                'amount' : "{:,.2f}".format(invoice.amount_total),
+                'date_payment' : date_payment.strftime("%d/%m/%Y"),
+                'days' : days,
+                'interest': "{:,.2f}".format(interest),
+                'interest_mo': "{:,.2f}".format(interest_mo),
+                'total': "{:,.2f}".format(total_inv)
+            }
+            inv_data.append(inv)
+        
+        total += credit.insurance
+        
+        data = {
+            'cycle' : credit.cycle,
+            'authorized' : "{:,.2f}".format(credit.authorized_amount),
+            'interest' : "{:,.2f}".format(credit.interest),
+            'sum_invoices' : "{:,.2f}".format(sum_invoices),
+            'sum_interest' : "{:,.2f}".format(sum_interest),
+            'total' : "{:,.2f}".format(total),
+            'date' : date_payment.strftime("%d/%m/%Y"),
+            'insurance' : "{:,.2f}".format(credit.insurance)
+        }
+
+
+        return {
+            'doc_ids': docids,
+            'doc_model': 'credit.preapplication',
+            'docs' : report,
+            'data' : data,
+            'invoices' : inv_data,
+            'company' : self.env.user.company_id
+        }
