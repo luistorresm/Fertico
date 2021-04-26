2ef30661f42c999e9eb53a6067b4403655ac7b5c
Noe Moreno <nmoreno@wobin.com.mx>
2021-04-26 11:07:33 -0500

    [FIX] version

--- wobin_credit/__manifest__.py
+++ wobin_credit/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1.2',
+    'version': '0.1.3',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'mail', 'web'],
