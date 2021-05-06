88a97366fdc4fd053e1f2dac0301c60fe696947a
Sebastian-AM-Wobin <smendez@wobin.com.mx>
2021-05-06 10:36:05 -0500

    [ADD] valores auxiliares related para recuperar name y nuevas validaciones

--- fertico-addons/models/account_add_receipt_file_field.py
+++ fertico-addons/models/account_add_receipt_file_field.py
@@ -4,4 +4,6 @@ class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
     receipt_payment_use_ids = fields.Many2many('ir.attachment', relation='receipt_payment_use_att_relation', string='Comprobante de Formas de Pago o Uso', track_visibility='always')
+    val_payment_term        = fields.Char(related='payment_term_id.name')
+    val_l10n_mx_edi_payment_method = fields.Char(related='l10n_mx_edi_payment_method_id.name')
     
