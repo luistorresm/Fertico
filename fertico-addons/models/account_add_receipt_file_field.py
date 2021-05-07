3e5dad19cf727bee911612f4fac19777fcbd5b77
Sebastian-AM-Wobin <smendez@wobin.com.mx>
2021-05-07 10:59:11 -0500

    [IMP] Cambio de field a tipo binary, cambio en condicionantes

--- fertico-addons/models/account_add_receipt_file_field.py
+++ fertico-addons/models/account_add_receipt_file_field.py
@@ -1,9 +1,24 @@
 from odoo import fields, models, api
 
+
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    receipt_payment_use_ids = fields.Many2many('ir.attachment', relation='receipt_payment_use_att_relation', string='Comprobante de Formas de Pago o Uso', track_visibility='always')
+    receipt_payment_use_ids = fields.Binary(string='Comprobante de Formas de Pago o Uso', track_visibility='always')
     val_payment_term        = fields.Char(related='payment_term_id.name')
-    val_l10n_mx_edi_payment_method = fields.Char(related='l10n_mx_edi_payment_method_id.name')
-    
+    flag_value_pay_term     = fields.Boolean(compute='_set_flag_value_pay_term')
+
+
+    @api.one
+    @api.depends('val_payment_term')
+    def _set_flag_value_pay_term(self):
+        if self.val_payment_term == 'AL.(PUE) Contado' :
+            self.flag_value_pay_term = True
+        elif self.val_payment_term == 'CL.(PUE) Contado':
+            self.flag_value_pay_term = True   
+        elif self.val_payment_term == 'LC.(PUE) Contado':
+            self.flag_value_pay_term = True     
+        elif self.val_payment_term == 'PI.(PUE) Contado':
+            self.flag_value_pay_term = True
+        elif self.val_payment_term == 'PM.(PUE) Contado':
+            self.flag_value_pay_term = True
