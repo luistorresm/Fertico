db70b83daae7ab1a763ffe99ee3b5bb5a71d107e
Noe Moreno <nmoreno@wobin.com.mx>
2021-06-16 09:44:33 -0500

    [FIX] modulos sobrantes

--- wobin_credit_limit/__manifest__.py
+++ wobin_credit_limit/__manifest__.py
@@ -29,17 +29,10 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'security/credit_security.xml',
 
         #'views/views.xml',
         #'views/templates.xml',
-        'views/block_credit.xml',
-        'views/tracking_limit.xml',
         'views/credit_limit.xml',
-        'views/res_partner_view.xml',
-        'views/account_invoice_view.xml',
-        'views/purchase_order_view.xml',
-        'views/sale_order_view.xml',
         
     ],
     # only loaded in demonstration mode
