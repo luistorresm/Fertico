6da659cba33f106ea44b88c8161320c786c0242c
Noe Moreno <nmoreno@wobin.com.mx>
2021-10-08 16:56:44 -0500

    [FIX] campos vauxoo

--- addons_14/models/__init__.py
+++ addons_14/models/__init__.py
@@ -2,7 +2,7 @@
 
 from . import models
 from . import migration
-from . import migration_fields_vauxoo
+#from . import migration_fields_vauxoo
 from . import pos_init
 from . import attendances
 from . import price_list
